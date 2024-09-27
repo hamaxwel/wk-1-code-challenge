b3fd24951ca5744a239a623992659b575f29c9c5
hamaxwel <157067741+hamaxwel@users.noreply.github.com>
2024-09-27 22:40:49 +0300

    speed.js

--- speed.js
+++ speed.js
@@ -1,6 +1,14 @@
 function speedDetector(speed) {
     const speedLimit = 70;
+ //setting the speed limit to be 70 
+    //set number of km per demerit point to 5 
 
+    
+    //use if else statement to check the driving speed against the speed limit.
+    // if the first condition is met, output "Ok"
+    //if the car speed exceeds the limit, calculate the number of points based on the km.
+    //if the points calculated exceeds 12, console.log "license suspended"
+    
     // Check if you are below the speed limit
     if (speed < speedLimit) {
         console.log("Ok");
